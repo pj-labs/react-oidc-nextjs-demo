b146025a3ae9dd99e7ad37cfbd4bbbdb29f5ed38
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2023-08-25 17:39:48 -0400

    Add Prettier as dev dependency for code formatting
    Prettier package is added to package.json for development purposes to enforce consistency in code style and format across the project. Accordingly, a new file .prettierrc is added to set rules for Prettier to use.
    
    Moreover, unnecessary type 'MyAppProps' is removed from _app.tsx and spaces are added between import braces for better looking and code readability.
    
    Finally, the pnpm lock file is updated to match new dependencies.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,16 @@
-import "@/styles/globals.css";
-import {SessionProvider} from "next-auth/react";
-import {AppProps} from "next/app";
+import '@/styles/globals.css';
+import { SessionProvider } from 'next-auth/react';
+import { AppProps } from 'next/app';
 
-interface MyAppProps extends AppProps {
-}
+export default function App(props: AppProps) {
+	const {
+		Component,
+		pageProps: { session, ...pageProps },
+	} = props;
 
-export default function App(props: MyAppProps) {
-    const {
-        Component,
-        pageProps: {session, ...pageProps},
-    } = props;
-
-    return (
-        <SessionProvider session={session}>
-            <Component {...pageProps} />
-        </SessionProvider>
-    );
+	return (
+		<SessionProvider session={session}>
+			<Component {...pageProps} />
+		</SessionProvider>
+	);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,30 @@
-import {Inter} from 'next/font/google'
-import {signIn, useSession} from "next-auth/react";
-import {useEffect} from 'react';
-
-const inter = Inter({subsets: ["latin"]});
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 export default function Home() {
-    const {data: session} = useSession();
+	const { data: session } = useSession();
 
-    useEffect(() => {
-        if (!session) {
-            signIn('credentials');
-        }
-    }, [session, window]);
+	function getComponent() {
+		if (session) {
+			return (
+				<>
+					Status: Logged in as {session?.user?.email}
+					<button type={'button'} onClick={() => signOut()}>
+						Log out
+					</button>
+				</>
+			);
+		}
+		return (
+			<>
+				Status: Not logged in <br />
+				<button onClick={() => signIn()}>Log in</button>
+			</>
+		);
+	}
 
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-        <h1>Welcome Home</h1>
-    </main>
-  );
+	return (
+		<>
+			<main className={`flex min-h-screen flex-col items-center justify-between p-24`}>{getComponent()}</main>
+		</>
+	);
 }
