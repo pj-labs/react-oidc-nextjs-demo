72e81f839ce46558781172c625a20de036da6da6
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2023-08-26 23:08:51 -0400

    Add Prettier as dev dependency for code formatting
    Prettier package is added to package.json for development purposes to enforce consistency in code style and format across the project. Accordingly, a new file .prettierrc is added to set rules for Prettier to use.
    
    Moreover, unnecessary type 'MyAppProps' is removed from _app.tsx and spaces are added between import braces for better looking and code readability.
    
    Finally, the pnpm lock file is updated to match new dependencies.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,10 @@ import { signIn, signOut, useSession } from 'next-auth/react';
 export default function Home() {
 	const { data: session } = useSession();
 
+	if (!session && typeof window !== 'undefined') {
+		signIn();
+	}
+
 	function getComponent() {
 		if (session) {
 			return (
