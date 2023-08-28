192a9b64f5050dcef06d8cda473ed4de14f4583b
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2023-08-28 12:35:58 -0400

    Updated authentication providers and secret handling in NextAuth configuration
    The diff introduces significant changes to the NextAuth configuration in `.env` and `api/auth/[...nextauth].ts` files. A secret environment variable that was hardcoded in `.env` file before is now removed, emphasizing the usage of environment variables for such sensitive data. The setup of OAuth providers in `api/auth/[...nextauth].ts` is also modified. Now there are two OAuth providers - Github and NCATS, both properly configured with client

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,49 +1,79 @@
-import NextAuth from "next-auth";
+import NextAuth from 'next-auth';
 
 export default NextAuth({
-  secret: process.env.SECRET,
-  providers: [
-    {
-      id: "ncats-next-auth-test",
-      clientId: "next-auth-test",
-      clientSecret: "NyUwABS68lTZq1lg1DzbE",
-      name: "NCATS",
-      type: "oauth",
-      wellKnown: "https://auth.ncats.nih.gov/_api/v2/auth/NCI-CCR-TEST/.well-known/openid-configuration",
-      //token: "https://auth.ncats.nih.gov/_api/v2/auth/NCI-CCR-TEST/oidc/token",
-      //userinfo: "https://auth.ncats.nih.gov/_api/v2/auth/NCI-CCR-TEST/me",
-      authorization: {
-        //url: "https://auth.ncats.nih.gov/_api/v2/auth/NCI-CCR-TEST/authorize",
-        params: {scope: "openid profile email offline"},
-      },
-      //issuer: "https://auth.ncats.nih.gov/_api/v2/auth/NCI-CCR-TEST",
-      profile(profile) {
-        console.log('profile ', profile)
-        return {
-          id: profile.sub,
-          name: profile.name,
-          email: profile.email,
-        };
-      },
-    }
-  ],
-  debug: true,
-  callbacks: {
-    async signIn({user, account, profile, email, credentials}) {
-      console.log('user', user, account, profile)
-      return true
-    },
-    async redirect({url, baseUrl}) {
-      return baseUrl
-    },
-    async session({session, token, user}) {
-      return session
-    },
-    async jwt({token, user, account, profile, isNewUser}) {
-      if (account?.accessToken) {
-        token.accessToken = account.accessToken
-      }
-      return token
-    },
-  },
+	providers: [
+		/*GithubProvider({
+			clientId: 'd6ca8737e1b3321d6ae7',
+			clientSecret: '6976ce59f65264945ef829b0d28fe63f2d3aaf17',
+		}),*/
+		{
+			id: 'd6ca8737e1b3321d6ae7',
+			clientId: 'd6ca8737e1b3321d6ae7',
+			clientSecret: '6976ce59f65264945ef829b0d28fe63f2d3aaf17',
+			name: 'Github',
+			type: 'oauth',
+			token: 'https://github.com/login/oauth/access_token',
+			userinfo: 'https://api.github.com/user',
+			idToken: false,
+			//token: "https://auth.ncats.nih.gov/_api/v2/auth/NCI-CCR-TEST/oidc/token",
+			//userinfo: "https://auth.ncats.nih.gov/_api/v2/auth/NCI-CCR-TEST/me",
+			authorization: {
+				url: 'https://github.com/login/oauth/authorize',
+				params: { scope: 'openid profile email offline' },
+			},
+			//issuer: "https://auth.ncats.nih.gov/_api/v2/auth/NCI-CCR-TEST",
+			profile(profile) {
+				console.log('profile ', profile);
+				return {
+					id: profile.id,
+					name: profile.name,
+					email: profile.email,
+				};
+			},
+		},
+		{
+			id: 'next-auth-demo',
+			clientId: 'next-auth-demo',
+			clientSecret: '73bf8dc7-43e0-48d9-862a-ceaef766c97a',
+			name: 'NCATS',
+			type: 'oauth',
+			idToken: true,
+			wellKnown: 'https://auth.ncats.nih.gov/_api/v2/auth/NCI-CCR-TEST/.well-known/openid-configuration',
+			//token: "https://auth.ncats.nih.gov/_api/v2/auth/NCI-CCR-TEST/oidc/token",
+			//userinfo: "https://auth.ncats.nih.gov/_api/v2/auth/NCI-CCR-TEST/me",
+			/*authorization: {
+				url: "https://auth.ncats.nih.gov/_api/v2/auth/NCI-CCR-TEST/authorize",
+				params: { scope: 'openid profile email' },
+			},*/
+			//issuer: "https://auth.ncats.nih.gov/_api/v2/auth/NCI-CCR-TEST",
+			profile(profile) {
+				console.log('profile:', profile);
+				return {
+					id: profile.sub,
+					name: profile.name,
+					email: profile.email,
+				};
+			},
+		},
+	],
+	debug: true,
+	secret: process.env.SECRET,
+	callbacks: {
+		async signIn({ user, account, profile, email, credentials }) {
+			console.log('user', user, account, profile);
+			return true;
+		},
+		async redirect({ url, baseUrl }) {
+			return baseUrl;
+		},
+		async session({ session, token, user }) {
+			return session;
+		},
+		async jwt({ token, user, account, profile, isNewUser }) {
+			if (account?.accessToken) {
+				token.accessToken = account.accessToken;
+			}
+			return token;
+		},
+	},
 });
